f728283a9ea999d90ebe90ceb1303f31239247db
ZaiiiRan <a1b075753@gmail.com>
2025-11-14 23:24:39 +0300

    token domain

--- auth-service/internal/repositories/models/v1_refresh_token.go
+++ auth-service/internal/repositories/models/v1_refresh_token.go
@@ -1,12 +1,33 @@
 package models
 
+import (
+	"time"
+
+	"github.com/ZaiiiRan/job_search_service/auth-service/internal/domain/token"
+)
+
 type V1RefreshTokenDal struct {
-	Id        int64  `db:"id" json:"id"`
-	UserId    int64  `db:"user_id" json:"user_id"`
-	Token     string `db:"token" json:"token"`
-	ExpiresAt int64  `db:"expires_at" json:"expires_at"`
-	CreatedAt int64  `db:"created_at" json:"created_at"`
-	UpdatedAt int64  `db:"updated_at" json:"updated_at"`
+	Id        int64     `db:"id" json:"id"`
+	UserId    int64     `db:"user_id" json:"user_id"`
+	Token     string    `db:"token" json:"token"`
+	ExpiresAt time.Time `db:"expires_at" json:"expires_at"`
+	CreatedAt time.Time `db:"created_at" json:"created_at"`
+	UpdatedAt time.Time `db:"updated_at" json:"updated_at"`
+}
+
+func V1RefreshTokenDalFromDomain(t *token.Token) V1RefreshTokenDal {
+	if t == nil {
+		return V1RefreshTokenDal{}
+	}
+
+	return V1RefreshTokenDal{
+		Id:        t.Id(),
+		UserId:    t.UserId(),
+		Token:     t.Token(),
+		ExpiresAt: t.ExpiresAt(),
+		CreatedAt: t.CreatedAt(),
+		UpdatedAt: t.UpdatedAt(),
+	}
 }
 
 func (p V1RefreshTokenDal) IsNull() bool { return false }
@@ -27,4 +48,12 @@ func (p V1RefreshTokenDal) Index(i int) any {
 	default:
 		return nil
 	}
+}
+
+func (p V1RefreshTokenDal) ToDomain() *token.Token {
+	return token.FromStorage(
+		p.Id, p.UserId,
+		p.Token, "refresh",
+		p.ExpiresAt, p.CreatedAt, p.UpdatedAt,
+	)
 }
